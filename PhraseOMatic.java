ad796925f9ff3296a424b9c67998af905c45ebbb
sdoty <51214759+sdoty@users.noreply.github.com>
2019-08-17 00:22:41 -0700

    Add files via upload

---
+++ PhraseOMatic.java
@@ -0,0 +1,8 @@
+public class PhraseOMatic {
+    public static void main (String[] args) {
+
+//make three sets of words to choose from.
+
+String[] wordListOne = {"24/7", "multi-Tier", "30,000 foot", "B-to-B", "win-win", "front-end", "web-based", "Agile", "Pervasive", "smart", "six-sigma", "critical-path", "dynamic", "Lightning"};
+
+String[] wordListTwo = {"empowered", "sticky", "value-added", "oriented", "centric","distributed", "clustered", "branded", "outside-the-box",
---
+++ TapeDeck.java
@@ -0,0 +1,34 @@
+class TapeDeck  {
+
+    boolean canRecord = false;
+
+    void playTape ()  {
+       System.out.println("tape Playing");
+
+    }
+
+    void recordTape()  {
+
+       System.out.println("tape recording");
+
+     }
+
+}
+
+class TapeDeckTestDrive  {
+    public static void main (String[] args)  {
+
+     TapeDeck t = new TapeDeck();
+        t.canRecord = true;
+	t.playTape();
+
+
+        if(t.canRecord == true)  {
+           t.recordTape();
+
+        }
+     }
+
+}
+
+
