959ac64bd063ce17c22c4961cddd573636996648
sdoty <51214759+sdoty@users.noreply.github.com>
2019-08-17 10:22:36 -0700

    Update MyNextApp.java

--- MyNextApp.java
+++ MyNextApp.java
@@ -7,7 +7,7 @@ public class MyNextApp {
         int roll = (int) (Math.random() * 7);
 
 	if (roll == 0) {
-	   roll = (int)(Math.random() * 7);  
+	   roll = (int)(Math.random() * 7);  // this line isn't doing anything.  Need syntax to create a loop so that it will roll until <> 0
            System.out.println("Please roll again.");
 	 } else {
            System.out.println("The roll is " + roll);
