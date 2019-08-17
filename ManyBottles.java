b02b1ae3038b7a8fd804b5af838837a5b5293820
sdoty <51214759+sdoty@users.noreply.github.com>
2019-08-17 00:21:43 -0700

    Create ManyBottles.java

---
+++ ManyBottles.java
@@ -0,0 +1,22 @@
+public class ManyBottles {
+	public static void main (String[] args) {
+
+		int b = 99;
+		while (b >  0) {
+		System.out.println (b+" bottles of beer on the wall,");
+		System.out.println (b+" bottles of beer,");
+		System.out.println ("Take one down, Pass it around,");
+		System.out.println (b-1+" bottles of beer on the wall!");
+		System.out.println();
+		b = b - 1;
+		
+	        }
+	
+	if (b == 0);
+		System.out.println (b+" bottles of beer on the wall");
+		System.out.println (b+" bottles of beer,");
+		System.out.println ("Now we are all really hammered!");
+	}
+
+}
+		
